8a947886caa65209b77939f97bb8c27dcf753a5e
jdenais <denais.jeremy@gmail.com>
2018-06-26 18:12:28 +0100

    Clean test application a bit

--- app/src/main/java/com/example/denais/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/denais/testapplication/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.denais.testapplication
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Button
 import com.example.denais.viewshaper.shapedlayout.ViewShaper
 
@@ -13,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.fragment_list)
+        setContentView(R.layout.activity_main)
 
         val ticket = findViewById<ViewShaper>(R.id.ticket)
 
